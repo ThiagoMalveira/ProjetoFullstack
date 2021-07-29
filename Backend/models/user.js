e84e7e17e8bef25440d81e99c19249015df8be89
ThiagoMalveira <thiago.a.malveira@gmail.com>
2021-07-28 22:47:22 -0300

    Primeiro Commit

---
+++ Backend/models/user.js
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose'
+import crypto from 'crypto'
+
+const schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+      select: false,
+      set: value =>
+        crypto
+          .createHash('md5')
+          .update(value)
+          .digest('hex'),
+    },
+    role: {
+      type: String,
+      required: true,
+    }
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true, getters: true },
+    toObject: { virtuals: true, getters: true },
+  }
+)
+export const UserModel = mongoose.model('User', schema)
